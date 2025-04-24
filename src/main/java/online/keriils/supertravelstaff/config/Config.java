07baa0c332442c9f2e6e7e0f24ce9ac065401e05
DauphalXir <145732644+Keriils@users.noreply.github.com>
2025-04-24 10:04:12 +0800

    clean

---
+++ src/main/java/online/keriils/supertravelstaff/config/Config.java
@@ -0,0 +1,9 @@
+package online.keriils.supertravelstaff.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class Config {
+
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC = BUILDER.build();
+}
---
+++ src/main/java/online/keriils/supertravelstaff/main/SuperTravelStaff.java
@@ -0,0 +1,58 @@
+package online.keriils.supertravelstaff.main;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import online.keriils.supertravelstaff.config.Config;
+import online.keriils.supertravelstaff.item.SuperTravelStaffItem;
+
+@SuppressWarnings("unused")
+@Mod(SuperTravelStaff.MODID)
+public class SuperTravelStaff {
+
+    public static final String MODID = "supertravelstaff";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    // Deferred Register
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TAB = DeferredRegister
+        .create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    // Define Staff Item
+    public static final String SUPER_TRAVEL_STAFF_NAME = "super_travel_staff";
+    public static final RegistryObject<SuperTravelStaffItem> SuperStaffItem = ITEMS
+        .register(SUPER_TRAVEL_STAFF_NAME, SuperTravelStaffItem::new);
+
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_TAB.register(
+        "super_travel_staff_tab",
+        () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(
+                () -> SuperStaffItem.get()
+                    .getDefaultInstance())
+            .displayItems((parameters, output) -> output.accept(SuperStaffItem.get()))
+            .build());
+
+    public SuperTravelStaff() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get()
+            .getModEventBus();
+        ITEMS.register(modEventBus);
+        CREATIVE_TAB.register(modEventBus);
+        ModLoadingContext.get()
+            .registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+}
